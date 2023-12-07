bd6e1abe10289f940abd0d9cbd1ba4b2c816183e
rusira <rusirajayatilake96@gmail.com>
2023-12-07 13:44:17 +0530

    footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from './components/navbar';
 import Main from './pages/main';
+import Footer from './components/footer';
 
 function App() {
   return (
     <div>
       <Navbar />
       <Main />
+      <Footer />
     </div>
   );
 }
